3c47231c733d9abe7c85a675cd3eb6ac72352bef
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-02-01 15:50:55 -0800

    displayed tea data on the page

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,8 @@ var app = angular.module('app');
 
 app.controller('HomeController', function($scope, $http){
   $http.get('http://localhost:8000/js/data.json').then(function(data){
-    console.log(data);
+    console.log(data.data)
+    $scope.items = [];
+    $scope.items = data.data.data;
   });
 });

e0ee35119a8cbac8a882f5ed013f775e4bf112f6
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-02-01 18:22:21 -0800

    initial implementaion for filter by categories

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,17 @@
 var app = angular.module('app');
+app.controller('IndexController', function($scope, categories){
+  //get categories
+   categories.fetchCategories.then(function(data){
+     $scope.categories = data;
+   });
+});
 
-app.controller('HomeController', function($scope, $http, menuItems){
+app.controller('HomeController', function($scope, $http, menuItems, categories){
     $scope.items = [];
-    console.log(menuItems.data().then(function(data){
+    // get menu items
+    menuItems.data().then(function(data){
         $scope.items = data.items;
-    }));
+    });
 
 
 });
--- js/services.js
+++ js/services.js
@@ -1,12 +1,35 @@
 var app = angular.module('app');
 
+// service to get menuItems from data.json
 app.service('menuItems', function($http){
-  return { data: function(){
-    return $http.get('http://localhost:8000/js/data.json').then(function(data){
-      return {
-        items: data.data.data
-      };
-    });
+  return {
+    data: function(){
+      return $http.get('http://localhost:8000/js/data.json').then(function(data){
+        return {
+          items: data.data.data
+        };
+      });
+    }
   }
-}
+});
+
+//service to get categories possible from our menuItems
+app.service('categories', function(menuItems){
+  return {
+    fetchCategories: menuItems.data().then(function(data){
+      var categories = [];
+      // get all categories possible
+      for(var i = 0; i< data.items.length; i++){
+        categories = categories.concat(data.items[i].categories);
+      }
+      // remove all dublicates
+      var uniqueCategories = function(a) {
+          return a.reduce(function(p, c) {
+              if (p.indexOf(c) < 0) p.push(c);
+              return p;
+          }, []);
+        };
+
+      return uniqueCategories(categories);
+  })};
 });

3bc2a2642aef179a2bfdb9b65436386a6710b117
Shashik Banda <shashik.banda@gmail.com>
2016-02-03 11:11:43 -0800

    clicking checkout button redirects page to checkout page. data from home html page passes to checkout shopping cart page. displaying some data on checkout table

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,10 @@ var app = angular.module('app');
 
 
 app.controller('IndexController', function($scope){
-  // $scope.cartTotal = addToCart.uniqueItemCount;
-
-  // console.log($scope.cartTotal)
-   // categories.fetchCategories.then(function(data){
-   //   $scope.categories = data;
-   // });
+  //Index Controller Empty for now
 });
 
-app.controller('HomeController', function($scope, $http, menuItems, categories, addToCart){
+app.controller('HomeController', function($scope, $http, $location, menuItems, categories, addToCart){
     categories.fetchCategories.then(function(data){
      $scope.categories = data;
     });
@@ -31,12 +26,18 @@ app.controller('HomeController', function($scope, $http, menuItems, categories, 
     $scope.addToBag = function(item,q){
       var cartObj = {};
       cartObj.tea = item;
-      cartObj.quanity = q;
+      cartObj.quantity = q;
       addToCart.cart.push(cartObj)
       $scope.cartTotal += 1 * q;
+    }
+
+    $scope.checkout = function(){
+      console.log("checkout clicked")
+      $location.path('/checkout');
     }
 });
 
 app.controller('CheckoutController', function($scope, $http, menuItems, categories, addToCart){
-  console.log(addToCart.cart)
+  $scope.cartArray = addToCart.cart;
+  console.log($scope.cartArray)
 })
